de27650ce946498fda8dd8b6d30193884cc6daac
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-20 19:51:08 -0300

    implementando insert

--- projeto-demo-dao-jdbc/src/application/Program.java
+++ projeto-demo-dao-jdbc/src/application/Program.java
@@ -27,6 +27,15 @@ public class Program {
         list.forEach(System.out::println);
 
 
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg","Greg@gmail.com",new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserido! novo Id = " + newSeller.getId());
+        System.out.println(sellerDao.findById(newSeller.getId()));
+
+
+
+
 
     }
 }
