6eab810191ea6b24e28088bcd8613f3405e13c43
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-20 15:09:13 -0300

    Criando DepartmentDao e SellerDao

--- projeto-demo-dao-jdbc/src/application/Program.java
+++ projeto-demo-dao-jdbc/src/application/Program.java
@@ -10,7 +10,6 @@ public class Program {
 
         Department obj = new Department(1, "Books");
         System.out.println(obj);
-
         Seller seller = new Seller(21, "Bob","Bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
 
